ece1705ab9d542046d774a19b994734120eb0196
Sujan Lama <sujanlama2323@gmail.com>
2026-07-29 17:47:49 +0545

    setup Google OAuth

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls'),name='core')
+    path('',include('core.urls'),name='core'),
+    path('social-auth/',include('social_django.urls',namespace='social')) #Google OAuth
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
